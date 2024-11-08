6a23332d3f1f2aba6427f65d59b6d4dc30a29caf
DaryAkerman <daryakerman2002@gmail.com>
2024-11-08 18:43:59 +0200

    fixed app

--- app.py
+++ app.py
@@ -79,5 +79,6 @@ def handle_message(msg):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+
 
